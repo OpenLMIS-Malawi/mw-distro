3fc1b1aaae89af9333257e961e89576747a57991
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-07-24 12:58:20 +0200

    Revert "MW4-3: nginx will wait until consul is present"
    This reverts commit 8ff555c2634a663c203276d42c379f0d39537a35.

--- reporting/config/services/nginx/init.sh
+++ reporting/config/services/nginx/init.sh
@@ -3,16 +3,8 @@
 set -e
 
 openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
-
 cp -r /config/nginx/consul-template/* /etc/consul-template/
-
 echo -n '$NGINX_BASIC_AUTH_USER:' >> /etc/nginx/.htpasswd
 openssl passwd -apr1 $NGINX_BASIC_AUTH_PW >> /etc/nginx/.htpasswd
-
-while ! curl -f "http://consul:8500/v1/agent/self"; do
-    sleep 10;
-done
-
 sleep 120
-
 /home/run.sh
