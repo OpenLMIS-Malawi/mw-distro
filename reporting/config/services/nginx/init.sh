f121bb6f8eb397bc60cd24ae1ad34346c5250326
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-07-24 12:58:50 +0200

    MW4-3: Increase wait time in nginx init script

--- reporting/config/services/nginx/init.sh
+++ reporting/config/services/nginx/init.sh
@@ -3,8 +3,12 @@
 set -e
 
 openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
+
 cp -r /config/nginx/consul-template/* /etc/consul-template/
+
 echo -n '$NGINX_BASIC_AUTH_USER:' >> /etc/nginx/.htpasswd
 openssl passwd -apr1 $NGINX_BASIC_AUTH_PW >> /etc/nginx/.htpasswd
-sleep 120
+
+sleep 240
+
 /home/run.sh
