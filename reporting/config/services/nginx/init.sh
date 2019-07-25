a8b48d0a2981bab33512b30bde208148e9241634
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-07-25 09:28:04 +0200

    MW4-3: Fixed issue with incorrect username in nginx config

--- reporting/config/services/nginx/init.sh
+++ reporting/config/services/nginx/init.sh
@@ -6,9 +6,9 @@ openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
 
 cp -r /config/nginx/consul-template/* /etc/consul-template/
 
-echo -n '$NGINX_BASIC_AUTH_USER:' >> /etc/nginx/.htpasswd
-openssl passwd -apr1 $NGINX_BASIC_AUTH_PW >> /etc/nginx/.htpasswd
+echo -n "${NGINX_BASIC_AUTH_USER}:" >> /etc/nginx/.htpasswd
+openssl passwd -apr1 ${NGINX_BASIC_AUTH_PW} >> /etc/nginx/.htpasswd
 
-sleep 240
+sleep 120
 
 /home/run.sh
