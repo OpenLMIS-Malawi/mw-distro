302bb7b71be4b19668d49c052d1895dac15a2408
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-07-30 09:28:02 +0200

    MW4-3: Added default logback.xml for NiFi Service

--- reporting/config/services/nifi/scripts/start.sh
+++ reporting/config/services/nifi/scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp /config/nifi/libs/* lib/ &&
+cp /config/nifi/conf/* conf &&
 /config/nifi/scripts/download-toolkit.sh $1 &&
 /config/nifi/scripts/preload.sh init $1 &&
 cd /opt/nifi/nifi-$1 &&
