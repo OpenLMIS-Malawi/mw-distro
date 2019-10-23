e30ad371af03fe9aa6b621f209c77f61dbb5b4df
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-10-23 12:47:03 +0200

    MW4-107: create custom file before nifi is started

--- reporting/config/services/nifi/scripts/preload.sh
+++ reporting/config/services/nifi/scripts/preload.sh
@@ -49,40 +49,9 @@ waitNifiAvailable() {
 }
 
 initialize() {
-  createOpenLmisProperties "$@"
   createRegClients "$@"
   importProcessGroups "$@"
   restartFlows "$@"
-}
-
-updateProperty() {
-  grep -q "^${1}" ${3} && sed -i "s|^${1}=.*$|${1}=${2}|" ${3} || echo "${1}=${2}" >> ${3}
-}
-
-createOpenLmisProperties() {
-  CONFIG="./conf/nifi.properties"
-  FILE="./conf/openlmis.properties"
-
-  if [ ! -f ${FILE} ];
-  then
-    touch ${FILE}
-  fi
-
-  updateProperty "nifi.variable.registry.properties" "${FILE}" "${CONFIG}"
-
-  updateProperty "openlmis.baseUrl" "${OL_BASE_URL}" "${FILE}"
-  updateProperty "openlmis.trustedHostname" "${TRUSTED_HOSTNAME}" "${FILE}"
-
-  updateProperty "openlmis.admin.username" "${OL_ADMIN_USERNAME}" "${FILE}"
-  updateProperty "openlmis.admin.password" "${OL_ADMIN_PASSWORD}" "${FILE}"
-
-  updateProperty "openlmis.authServer.clientId" "${AUTH_SERVER_CLIENT_ID}" "${FILE}"
-  updateProperty "openlmis.authServer.clientSecret" "${AUTH_SERVER_CLIENT_SECRET}" "${FILE}"
-
-  updateProperty "openlmis.fhir.id" "${FHIR_ID}" "${FILE}"
-  updateProperty "openlmis.fhir.password" "${FHIR_PASSWORD}" "${FILE}"
-
-  updateProperty "openlmis.postgres.password" "${POSTGRES_PASSWORD}" "${FILE}"
 }
 
 createRegClients() {
