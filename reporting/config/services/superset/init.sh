13d815d5cf4ca971fb7b7a78973e4bacd8e268ec
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-10-30 10:12:40 +0100

    MW4-79: Increased superset timeout

--- reporting/config/services/superset/init.sh
+++ reporting/config/services/superset/init.sh
@@ -23,4 +23,4 @@ superset import_datasources -p $CONFIG_DIR/datasources/database.yaml
 superset import_dashboards -p $CONFIG_DIR/dashboards/openlmis_uat_dashboards.json
 superset init
 
-gunicorn -w 2 --timeout 60 -b 0.0.0.0:8088 --reload --limit-request-line 0 --limit-request-field_size 0 superset:app
+gunicorn -w 2 --timeout 900 -b 0.0.0.0:8088 --reload --limit-request-line 0 --limit-request-field_size 0 superset:app
