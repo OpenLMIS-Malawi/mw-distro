3f9327f8c0a5596e9ffb1e76792527514a4e302a
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2020-12-08 15:44:50 +0100

    MALAWISUP-2408: Added logs if basic auth is set and removed condition

--- reporting/config/services/nginx/init.sh
+++ reporting/config/services/nginx/init.sh
@@ -6,6 +6,9 @@ openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
 
 cp -r /config/nginx/consul-template/* /etc/consul-template/
 
+: ${NGINX_BASIC_AUTH_USER:?"Need to set NGINX_BASIC_AUTH_USER"}
+: ${NGINX_BASIC_AUTH_PW:?"Need to set NGINX_BASIC_AUTH_PW"}
+
 echo -n "${NGINX_BASIC_AUTH_USER}:" >> /etc/nginx/.htpasswd
 openssl passwd -apr1 ${NGINX_BASIC_AUTH_PW} >> /etc/nginx/.htpasswd
 
