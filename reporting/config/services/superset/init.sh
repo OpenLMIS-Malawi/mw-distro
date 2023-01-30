2bbba5f5885b7ae143eb10669bb6c9f0b7839d2e
sniedzielski <sniedzielski@soldevelo.com>
2023-01-30 17:00:13 +0100

    MW-1234: replace depreceated fabmanager by flask fab

--- reporting/config/services/superset/init.sh
+++ reporting/config/services/superset/init.sh
@@ -24,7 +24,7 @@ done
 >&2 echo "Postgres is up"
 
 # App initialization
-fabmanager create-admin --app superset --username ${SUPERSET_ADMIN_USERNAME} --firstname Admin --lastname Admin --email noreply --password ${SUPERSET_ADMIN_PASSWORD} &&
+flask fab create-admin --username ${SUPERSET_ADMIN_USERNAME} --firstname Admin --lastname Admin --email noreply --password ${SUPERSET_ADMIN_PASSWORD} &&
 
 superset db upgrade &&
 superset import_datasources -p $CONFIG_DIR/datasources/database.yaml &&
