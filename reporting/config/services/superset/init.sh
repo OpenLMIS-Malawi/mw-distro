dee62bb3719ffafd8e246c5595dd29e438fede04
sniedzielski <sniedzielski@soldevelo.com>
2023-01-31 17:05:39 +0100

    MW-1234: temporary make examples dashboard file empty

--- reporting/config/services/superset/init.sh
+++ reporting/config/services/superset/init.sh
@@ -28,7 +28,7 @@ flask fab create-admin --username ${SUPERSET_ADMIN_USERNAME} --firstname Admin -
 
 superset db upgrade &&
 superset import_datasources -p $CONFIG_DIR/datasources/database.yaml &&
-# superset import_dashboards -p $CONFIG_DIR/dashboards/openlmis_uat_dashboards.json &&
+superset import_dashboards -p $CONFIG_DIR/dashboards/openlmis_uat_dashboards.json &&
 superset init &&
 
 gunicorn -w 2 --timeout 900 -b 0.0.0.0:8088 --reload --limit-request-line 0 --limit-request-field_size 0 superset:app
